22105667eeb74ce0bda38ac4bd7157e25254d6c4
shivaram <shivarammittakola@gmail.com>
2024-08-03 10:55:57 +0530

    vercel

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Competitive Programmer" , "DSA Solver" ];
+  const toRotate = [ "Web Developer", "Competitive Coder" , "Coding Enthusiast" ];
   const period = 2000;
 
   useEffect(() => {
